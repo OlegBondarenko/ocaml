18786104c0ca85cce29b30529d3af08de3891528
Oleg <oleg.bondarenko@itechcraft.com>
2024-07-09 10:26:03 +0300

    adding enum to log data

--- bin/clogger.ml
+++ bin/clogger.ml
@@ -1,10 +1,21 @@
+type logType = Info | Error | Warning
 
 module Clogger = struct
+  let logTypeToString = function
+    | Info -> "Info"
+    | Error -> "Error"
+    | Warning -> "Warning"
+
   let test x = Format.printf "%s.\n" x
-  
-  let logInfo x =
+
+  let log (t : logType) (x : string) =
     let oc =
       open_out_gen [ Open_creat; Open_append; Open_nonblock ] 0o777 "file.txt"
     in
-    Printf.fprintf oc "[Info] %s.\n" x;     
+
+    Printf.fprintf oc "[%s] %s.\n" (logTypeToString t) x
+
+  let logInfo x = log Info x
+  let logWarning x = log Warning x
+  let logError x = log Error x
 end
